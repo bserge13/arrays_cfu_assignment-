f0d221cc4a419e6fa6fef06e00467ae920f2d52e
Blake Sergesketter <blakesergesketter@gmail.com>
2023-03-29 18:40:28 -0500

    add explinations to the commands

--- arrays.rb
+++ arrays.rb
@@ -1,8 +1,21 @@
 nfl_teams = ["Bears", "Titans", "Broncos", "Colts"]
+# Calling on number 0 will get the response, Bears, 
+# since that is first in the order. 
 puts nfl_teams[0]
+
 city_numbers = [34, 21, 420, 812]
+# Calling number 3 will get the response, 812, because
+# it is fourthin in line in the array. 
 puts city_numbers[3]
+
 win_odds = [18.675, 9.42, 6.789, 3.45]
+# Calling number 1 will get the response of, 9.42, 
+# since it is in the second spot of the array.
 puts win_odds[1]
+
 fun_times = [false, true, true, false]
+# Calling on number 2 will get the response, true,
+# as it is third in line in the array.  
 puts fun_times[2]
+
+
